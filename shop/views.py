d518eede328d2aadc7e84818076589a979ca2c20
jod35 <jod35@localhost.localdomain>
2019-10-09 14:25:34 +0300

    created the products page

--- shop/views.py
+++ shop/views.py
@@ -53,3 +53,32 @@ def login_page():
 def logout():
    logout_user()
    return redirect(url_for('login_page'))
+
+@app.route('/addproduct',methods=['POST'])
+def add_product():
+   prod_name=request.form.get('prod_name')
+   cost_price=request.form.get('cost_price')
+   mark_up=request.form.get('mark_up')
+   discount=request.form.get('discount')
+   stock=request.form.get('stock')
+   prod_type=request.form.get('prod_type')
+   
+   disc=cost_price - (int(discount /100) *cost_price)
+   seling_price = (cost_price + ((mark_up/100) * cost_price)) -disc
+
+   new_product=Product(
+      prod_name=prod_name,
+      cost_price=cost_price,
+      selling_price=seling_price,
+      discount=disc,
+      stock=stock,
+      prod_type=prod_type,
+      seller=current_user
+   )
+   db.session.add(new_product)
+   db.session.commit()
+   return redirect(url_for(add_product))
+
+
+
+   pass
